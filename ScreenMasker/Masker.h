44d7ba0c61c4d1675f68bf9819d7f6e46d710997
David Hull <scorpioman819@msn.com>
2019-02-16 13:27:58 -0800

    Add project files.

---
+++ ScreenMasker/Masker.h
@@ -0,0 +1,9 @@
+#pragma once
+static class Masker
+{
+public:
+
+
+
+};
+
---
+++ ScreenMasker/ScreenMasker.cpp
@@ -0,0 +1,12 @@
+
+#include "Control.h"
+
+void main(int argc, char* argv[])
+{
+	Control* ctrl = new Control();
+
+	
+
+	delete ctrl;
+	ctrl = nullptr;
+}
